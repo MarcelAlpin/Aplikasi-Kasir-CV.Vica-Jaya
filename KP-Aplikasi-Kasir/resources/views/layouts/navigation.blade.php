7f63813419ff42814cc2cb9c9bc8744f153af24a
MarcelAlpin <marcelalpinsanty_2226240044@mhs.mdp.ac.id>
2025-06-09 23:37:40 +0700

    Update navigation.blade.php

--- KP-Aplikasi-Kasir/resources/views/layouts/navigation.blade.php
+++ KP-Aplikasi-Kasir/resources/views/layouts/navigation.blade.php
@@ -1,28 +1,22 @@
 <nav x-data="{ open: false }" class="fixed top-0 left-0 w-full z-50 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex">
-            <div class="flex">
-
-                <!-- Sidebar Toggle -->
-                <div class="flex justify-content-lg-start">
-                    <!-- Toggle Button -->
-                    <button @click="$dispatch('toggle-sidebar')" class="text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white focus:outline-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    </button>
-                </div>
-
+    <div class="flex items-center h-16">
+        <!-- Sidebar Toggle - absolutely positioned at left -->
+        <button @click="$dispatch('toggle-sidebar')" class="p-4 text-gray-600 dark:text-gray-300 hover:text-gray-800 dark:hover:text-white focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+        </button>
+        
+        <!-- Rest of navigation content -->
+        <div class="flex-1 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-16">
                 <!-- Page Title -->
-                <div class="hidden sm:ml-36 sm:flex items-center">
+                <div class="flex items-center">
                     <h1 class="text-lg font-semibold text-gray-800 dark:text-gray-200">
                         {{ $header }}
                     </h1>
                 </div>
-            </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 
