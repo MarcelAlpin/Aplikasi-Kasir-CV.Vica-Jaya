12a2bbd04fcdb5f8bd2d6b41336bab6e594c541f
MarcelAlpin <marcelalpinsanty_2226240044@mhs.mdp.ac.id>
2025-06-09 23:34:10 +0700

    Update navigation.blade.php

--- KP-Aplikasi-Kasir/resources/views/layouts/navigation.blade.php
+++ KP-Aplikasi-Kasir/resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav x-data="{ open: false }" class="fixed top-0 left-0 w-full z-50 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
+        <div class="flex">
             <div class="flex">
 
                 <!-- Sidebar Toggle -->
