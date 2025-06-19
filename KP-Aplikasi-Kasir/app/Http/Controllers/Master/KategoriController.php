badec64ff23aaf43488df2fec5bbb7a35f9fd829
djalisuandre <160694379+djalisuandre@users.noreply.github.com>
2025-06-19 08:02:53 +0700

    Update KategoriController.php

--- KP-Aplikasi-Kasir/app/Http/Controllers/Master/KategoriController.php
+++ KP-Aplikasi-Kasir/app/Http/Controllers/Master/KategoriController.php
@@ -11,7 +11,7 @@ class KategoriController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         $search = $request->input('search');
     
