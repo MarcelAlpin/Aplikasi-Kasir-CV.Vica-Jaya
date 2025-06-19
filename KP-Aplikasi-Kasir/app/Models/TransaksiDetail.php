28b41366bd79042971ec0e5f969aa7479770b3d7
MarcelAlpin <marcelalpinsanty_2226240044@mhs.mdp.ac.id>
2025-06-19 23:30:13 +0700

    .

--- KP-Aplikasi-Kasir/app/Models/TransaksiDetail.php
+++ KP-Aplikasi-Kasir/app/Models/TransaksiDetail.php
@@ -1,29 +1,28 @@
 <?php
-// In App/Models/DetailTransaksi.php
 
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TransaksiDetail extends Model
 {
-    protected $table = 'detail_transaksi';
-    
-    protected $fillable = [
-        'transaksi_id',
-        'barang_id',
-        'qty',
-        'harga',
-        'subtotal'
-    ];
+    //
+    protected $table = 'transaksi_detail';
 
+    // Primary key
+    protected $primaryKey = 'id';
+    // Primary key bukan incrementing integer
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = ['id', 'transaksi_id','barang_id','harga','qty',];
+    public function barang()
+    {
+        return $this->belongsTo(Barang::class, 'barang_id');
+    }
     public function transaksi()
     {
         return $this->belongsTo(Transaksi::class);
-    }
-
-    public function barang()
-    {
-        return $this->belongsTo(Barang::class);
     }
 }
